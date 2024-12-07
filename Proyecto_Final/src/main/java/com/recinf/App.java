295d73506d5a18c836dd5cf810e5fd34d313a7d5
dr0pshot03 <100786770+dr0pshot03@users.noreply.github.com>
2024-12-07 13:52:12 +0100

    calculartf2

--- Proyecto_Final/src/main/java/com/recinf/App.java
+++ Proyecto_Final/src/main/java/com/recinf/App.java
@@ -1,10 +1,14 @@
 package com.recinf;
 
+import java.util.HashMap;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        Crawler.buscador("https://raw.githubusercontent.com/PdedP/RECINF-Project/refs/heads/main/index.html");
-        
+        //Crawler.buscador("https://raw.githubusercontent.com/PdedP/RECINF-Project/refs/heads/main/index.html");
+        filtros.main(args);
+        HashMap<String, HashMap<String, Integer>> termsFrecuencia = calcularTF1();
+        calcularTF2()
     }
 }
--- Proyecto_Final/src/main/java/com/recinf/calcularTF2.java
+++ Proyecto_Final/src/main/java/com/recinf/calcularTF2.java
@@ -1,5 +1,53 @@
 package com.recinf;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class calcularTF2 {
-    
+    public void funcionTF2(HashMap<String, HashMap<String, Integer>> termsFrecuencia)
+    {
+        HashMap<String, Tupla> indiceInvertido = new HashMap<>();
+        for(Map.Entry<String, HashMap<String, Integer>> i : termsFrecuencia.entrySet())
+        {
+            String archivo = i.getKey();
+            System.out.println("Archivo: "+archivo);
+            for(Map.Entry<String, Integer> j : i.getValue().entrySet())
+            {
+                double tf = 1 + Math.log(j.getValue()) / Math.log(2);
+
+                if (indiceInvertido.containsKey(j.getKey()))
+                {
+                    Tupla t = indiceInvertido.get(j.getKey());
+                    t.docIDPeso.put(archivo, tf);
+                    indiceInvertido.put(j.getKey(), t); // Vuelve a insertar la palabra
+                }
+                else{
+                    HashMap<String, Double> DOC_Peso = new HashMap<>();
+                    DOC_Peso.put(archivo, tf);
+                    Tupla t = new Tupla(0, DOC_Peso);
+                    indiceInvertido.put(j.getKey(), t);
+                }
+            }
+        }
+
+        for (Map.Entry<String, Tupla> entry : indiceInvertido.entrySet()) {
+            String termino = entry.getKey();
+            Tupla tupla = entry.getValue();
+            System.out.println("Término: " + termino);
+            System.out.println("IDF: " + tupla.idf);
+            for (Map.Entry<String, Double> docEntry : tupla.docIDPeso.entrySet()) {
+                System.out.println("Documento: " + docEntry.getKey() + ", Peso: " + docEntry.getValue());
+            }
+        }
+    }
+}
+
+class Tupla {
+    public double idf;
+    public HashMap<String, Double> docIDPeso;
+
+    public Tupla(double idf, HashMap<String, Double> docIDPeso) {
+        this.idf = idf;
+        this.docIDPeso = docIDPeso;
+    }
 }
