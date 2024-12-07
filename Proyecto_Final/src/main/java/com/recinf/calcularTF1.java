77eade676e40881399cfe7ba1f3b03d1f765c127
ruben2312 <127433122+ruben2312@users.noreply.github.com>
2024-12-07 14:09:13 +0100

    filtro

--- Proyecto_Final/src/main/java/com/recinf/calcularTF1.java
+++ Proyecto_Final/src/main/java/com/recinf/calcularTF1.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 
-public class calcurarTF1 {
+public class calcularTF1 {
 
     public static HashMap funcionTF1()
     {
